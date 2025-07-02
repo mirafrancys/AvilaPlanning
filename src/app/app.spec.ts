4bb39815c4dc1cfb05447913f467427de8a1b7c6
Sylva <sylvain@jardinsdelafrance.com>
2025-07-02 12:42:47 -0400

    Premier commit officiel pour ce projet

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -10,6 +10,7 @@ describe('App', () => {
     }).compileComponents();
   });
 
+  
   it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
